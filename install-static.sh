0f35d2308014afa2b64406e42f38eedf329e78df
liudevmodularo-ai <liu.dev.modularo@gmail.com>
2026-04-30 19:50:38 -0300

    samba_static

--- install-static.sh
+++ install-static.sh
@@ -61,69 +61,25 @@ print_success "Dependências instaladas."
 # --- CRIAÇÃO DA ESTRUTURA E DOWNLOADS ---
 print_header "📁 PREPARANDO ARQUIVOS DO SITE"
 
+# Verifica se os arquivos necessários estão presentes no pacote de deploy
+if [ ! -f "index.html" ] || [ ! -d "images" ]; then
+    print_error "Certifique-se de que 'index.html' e o diretório 'images/' estão no mesmo local que este script."
+    exit 1
+fi
+
 mkdir -p "$INSTALL_DIR/static/uploads"
 print_success "Diretório criado em $INSTALL_DIR"
 
-image_map=(
-    "https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511843/4.jpg gallery_01.jpg"
-    "https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511844/4.jpg gallery_02.jpg"
-    "https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511846/4.jpg gallery_03.jpg"
-    "https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511847/4.jpg gallery_04.jpg"
-    "https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511848/4.jpg gallery_05.jpg"
-    "https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511849/4.jpg gallery_06.jpg"
-    "https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511850/4.jpg gallery_07.jpg"
-    "https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511845/4.jpg gallery_08.jpg"
-    "https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511851/4.jpg gallery_09.jpg"
-    "https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511852/4.jpg gallery_10.jpg"
-    "https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511854/4.jpg gallery_11.jpg"
-    "https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511840/4.jpg gallery_12.jpg"
-    "https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511841/4.jpg gallery_13.jpg"
-    "https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511842/4.jpg gallery_14.jpg"
-    "https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511853/4.jpg gallery_15.jpg"
-    "https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/24819900/4.jpg gallery_16.jpg"
-)
+print_info "Copiando imagens para o servidor..."
+cp -r images/* "$INSTALL_DIR/static/uploads/"
+print_success "Imagens copiadas."
 
-print_info "Baixando imagens..."
-for item in "${image_map[@]}"; do
-    read -r url filename <<< "$item"
-    if [ ! -f "$INSTALL_DIR/static/uploads/$filename" ]; then
-        wget -q -O "$INSTALL_DIR/static/uploads/$filename" "$url"
-        echo "  - Baixado: $filename"
-    else
-        echo "  - Já existe: $filename"
-    fi
-done
-print_success "Download de imagens concluído."
+print_info "Copiando index.html para o servidor..."
+cp index.html "$INSTALL_DIR/index.html"
+print_success "index.html copiado."
 
 # --- CRIAÇÃO DO ARQUIVO HTML ---
-print_info "Gerando arquivo index.html..."
-
-# Copia o conteúdo do arquivo index.html local para a VPS
-# Certifique-se de que este script e o index.html estão no mesmo diretório ao copiar para a VPS
-if [ -f "index.html" ]; then
-    # Substitui os caminhos das imagens para serem relativos ao servidor estático
-    sed -e "s|https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511843/4.jpg|/static/uploads/gallery_01.jpg|g" \
-        -e "s|https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511844/4.jpg|/static/uploads/gallery_02.jpg|g" \
-        -e "s|https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511846/4.jpg|/static/uploads/gallery_03.jpg|g" \
-        -e "s|https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511847/4.jpg|/static/uploads/gallery_04.jpg|g" \
-        -e "s|https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511848/4.jpg|/static/uploads/gallery_05.jpg|g" \
-        -e "s|https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511849/4.jpg|/static/uploads/gallery_06.jpg|g" \
-        -e "s|https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511850/4.jpg|/static/uploads/gallery_07.jpg|g" \
-        -e "s|https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511845/4.jpg|/static/uploads/gallery_08.jpg|g" \
-        -e "s|https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511851/4.jpg|/static/uploads/gallery_09.jpg|g" \
-        -e "s|https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511852/4.jpg|/static/uploads/gallery_10.jpg|g" \
-        -e "s|https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511854/4.jpg|/static/uploads/gallery_11.jpg|g" \
-        -e "s|https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511840/4.jpg|/static/uploads/gallery_12.jpg|g" \
-        -e "s|https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511841/4.jpg|/static/uploads/gallery_13.jpg|g" \
-        -e "s|https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511842/4.jpg|/static/uploads/gallery_14.jpg|g" \
-        -e "s|https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/23511853/4.jpg|/static/uploads/gallery_15.jpg|g" \
-        -e "s|https://dbvebtn3wsif3.cloudfront.net/foto/3591/3591/v-15/imoveis/1473925/24819900/4.jpg|/static/uploads/gallery_16.jpg|g" \
-        index.html > "$INSTALL_DIR/index.html"
-    print_success "Arquivo index.html criado e caminhos de imagem atualizados."
-else
-    print_error "Arquivo 'index.html' não encontrado no diretório atual. Abortando."
-    exit 1
-fi
+# A seção de criação de HTML foi removida, pois o index.html agora é copiado diretamente.
 
 # --- CONFIGURAÇÃO DO NGINX ---
 print_header "🌐 CONFIGURANDO NGINX"
